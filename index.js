e7943f4fa85065fa9d9d128e9cf889f209c558b5
Tom Wilson <tom@jackhq.com>
2016-04-28 23:13:07 -0400

    refactored service

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const on = require('./lib/handle-events')
 const uidPath = ['object', 'userId']
 const dataPath = ['object', 'userData']
 
+const auth0get = '/auth0/user/get'
+const auth0update = '/auth0/user/update'
 
 // pure functions
 const notValidId = pathSatisfies(isNil, uidPath)
@@ -14,8 +16,8 @@ const notValidData = pathSatisfies(isNil, dataPath)
 module.exports = curry((config, ee) => {
   const { getUser, updateUser } = new Auth0(config)
 
-  on(ee, '/auth0/user/get', getUser, [uidPath], notValidId)
+  on(ee, auth0get, getUser, [uidPath], notValidId)
 
-  on(ee, '/auth0/user/update', updateUser, [uidPath, dataPath], notValidData)
+  on(ee, auth0update, updateUser, [uidPath, dataPath], notValidData)
 
 })
