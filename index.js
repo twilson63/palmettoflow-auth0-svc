2c974215fb37db0d66197f9c3b213d59b821057a
Tom Wilson <tom@jackhq.com>
2016-04-29 03:39:38 -0400

    refactored to use event streams with most

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-var Auth0 = require('auth0')
+const { fromEvent, fromPromise } = require('most')
+const { curry, path } = require('ramda')
+const { response, responseError }  = require('palmettoflow-event')
 
-const { isNil, pathSatisfies, curry } = require('ramda')
-const on = require('./lib/handle-events')
-
-const uidPath = ['object', 'userId']
-const dataPath = ['object', 'userData']
-
-const auth0get = '/auth0/user/get'
-const auth0update = '/auth0/user/update'
-
-// pure functions
-const notValidId = pathSatisfies(isNil, uidPath)
-const notValidData = pathSatisfies(isNil, dataPath)
-
-module.exports = curry((config, ee) => {
+const svc = curry((config, ee) => {
   const { getUser, updateUser } = new Auth0(config)
+  const send = ev => ee.emit('send', ev)
 
-  on(ee, auth0get, getUser, [uidPath], notValidId)
+  fromEvent('/auth0/user/get', ee)
+    .flatMap(handleGetUser)
+    .observe(send)
 
-  on(ee, auth0update, updateUser, [uidPath, dataPath], notValidData)
+  fromEvent('/auth0/user/update', ee)
+    .flatMap(handleUpdateUser)
+    .observe(send)
 
+  return ee
+
+  function handleGetUser (event) {
+    const userId = path(['object','userId'], event) || null
+
+    return fromPromise(getUser(userId)
+      .then(data => response(data, event))
+      .catch(err => responseError(err, event))
+    )
+  }
+
+  function handleUpdateUser (event) {
+    const userId = path(['object','userId'], event) || null
+    const userData = path(['object','userData'], event) || null
+
+    return fromPromise(updateUser(userId, userData)
+      .then(data => response(data, event))
+      .catch(err => responseError(err, event))
+    )
+  }
 })
+
+// export svc
+module.exports = svc
